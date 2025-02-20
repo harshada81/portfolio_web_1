176803f7fad29d8d85536c6fb9e4db166ecc8cda
harshada81 <harshadakedare81@gmail.com>
2025-02-20 11:37:48 +0530

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var util = require("util");
 var conn = mysql.createConnection({
-    "host":"localhost",
-    "user":"root",
-    "password":"",
-    "database":"portfolio"
+    "host":"bmh07zjyflwrcmnuxsfb-mysql.services.clever-cloud.com",
+    "user":"u2llqjnc4k841z21",
+    "password":"YkTiYGsBSx8McWyzcThu",
+    "database":"bmh07zjyflwrcmnuxsfb"
 });
 
 var exe =util.promisify(conn.query).bind(conn);
