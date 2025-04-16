9cae3862cbbe63286d09164207a293b29a891de6
harshada81 <harshadakedare81@gmail.com>
2025-04-16 14:37:08 +0530

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var util = require("util");
 var conn = mysql.createConnection({
-    "host":"bmh07zjyflwrcmnuxsfb-mysql.services.clever-cloud.com",
-    "user":"u2llqjnc4k841z21",
-    "password":"YkTiYGsBSx8McWyzcThu",
-    "database":"bmh07zjyflwrcmnuxsfb"
+    "host":"bjgtaz2ujogpmvhkpdau-mysql.services.clever-cloud.com",
+    "user":"uyn6js7gwi7ozat8",
+    "password":"6WlguFW7J9Kr7nlTSbVV",
+    "database":"bjgtaz2ujogpmvhkpdau"
 });
 
 var exe =util.promisify(conn.query).bind(conn);
